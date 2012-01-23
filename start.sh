e19f5a5e8b142e2a3d49d96f11e7494f81702595
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-01-23 21:16:06 +0100

    version

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 nohup mex-server 2012 passwd.real +RTS -M100m &
+
+# demo client name and password must be last line of password file
 nohup mex-client $(tail -1 passwd.real) http://localhost:3000 http://localhost:2012/rpc +RTS -M100m &
+
