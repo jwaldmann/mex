c294746de67e7bd0da51705ae3b346cdffc670f4
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-01-23 21:02:18 +0100

    change

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-nohup mex-server 2012 passwd +RTS -M100m &
-nohup mex-client L L http://localhost:3000 http://localhost:2012/rpc +RTS -M100m &
-
+nohup mex-server 2012 passwd.real +RTS -M100m &
+nohup mex-client $(tail -1 passwd.real) http://localhost:3000 http://localhost:2012/rpc +RTS -M100m &
